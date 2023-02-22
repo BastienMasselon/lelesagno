ba5026d6c80ac4a3a7b5dae90580b49e8cb5051e
Bastien M <bastien.masselon@laposte.net>
2023-02-22 15:37:59 +0100

    Création et import du composant Header

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
 
+// Components
+import Header from './components/Header';
+
 function App() {
   return (
     <div className="App">
-      <h1 className="bg-sky-500/50">hello Lele!</h1>
+      <Header />
     </div>
   );
 }
---
+++ front/src/components/Header.js
@@ -0,0 +1,13 @@
+// == Import
+
+// == Composant
+function Header() {
+  return (
+    <div className="header">
+      <h1 className="bg-sky-500/50">hello Lele! :)</h1>
+    </div>
+  );
+}
+
+// == Export
+export default Header;
