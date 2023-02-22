657ecd64fdc94fac8387af36cfdd1026ca905d90
Bastien M <bastien.masselon@laposte.net>
2023-02-22 16:37:08 +0100

    Installation et débug de ESLint

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -4,7 +4,7 @@
 function Header() {
   return (
     <div className="header">
-      <h1 className="bg-sky-500/50">hello Lele! :)</h1>
+      <h1 className="bg-sky-500/50">hello Lele! :) </h1>
     </div>
   );
 }
